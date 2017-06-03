0efa82a3b7636cccfd42fc0697b05730f48a454f
magdate <magdalena.terefenko@gmail.com>
2017-06-03 22:51:28 +0200

    Renamed Schedule to Agenda

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import {Route} from "react-router-dom";
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Contacts from "./Components/Contacts/Contacts";
-import Schedule from "./Components/Schedule/Schedule";
+import Agenda from "./Components/Agenda/Agenda";
 import Ticket from "./Components/Ticket/Ticket";
 import Scanner from "./Components/Scanner/Scanner";
 
@@ -14,7 +14,7 @@ export default {
         <Route key="1" exact path="/" component={Home}/>,
         <Route key="2" path="/about" component={About} label="About"/>,
         <Route key="3" path="/contacts" component={Contacts} label="Contacts"/>,
-        <Route key="4" path="/schedule" render={(props) => <Schedule {...props} userName="Dan"/>} label="Schedule"/>,
+        <Route key="4" path="/agenda" render={(props) => <Agenda {...props} userName="Dan"/>} label="Agenda"/>,
         <Route key="5" path="/scanner" render={(props) => <Scanner {...props} onScaned={console.log}/>} label="Scanner"/>,
         <Route key="7" path="/ticket" render={(props) => <Ticket {...props} name="Bartosz"/>} label="Ticket"/>,
     ]
