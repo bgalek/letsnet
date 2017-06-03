028cda6753723f5d8961a616317e0b051e31f009
Bartosz Galek <bartosz.galek@allegrogroup.com>
2017-06-03 09:46:16 +0200

    material ui

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './About.css';
+
+class About extends Component {
+    render() {
+        return (
+            <p>about us!</p>
+        );
+    }
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import logo from './logo.svg';
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+injectTapEventPlugin();
+
+const app =
+    <Router>
+        <MuiThemeProvider>
+            <div className="App">
+                <AppBar title="Let's net" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Let&apos;s net!</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                    </ul>
+                    <hr/>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                </div>
+            </div>
+        </MuiThemeProvider>
+    </Router>;
+
+ReactDOM.render(app, document.getElementById('root'));
+
 registerServiceWorker();
