02346e1ea5c3e91aeff2d7ba178189f4d3f87a8c
Krzysztof Szporer <krzysiek.szporer@gmail.com>
2017-06-04 12:55:16 +0200

    Started with menu redesign.

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Col, Row, Grid} from "react-flexbox-grid";
 import './TopBar.css';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
@@ -6,6 +7,16 @@ import MenuItem from 'material-ui/MenuItem';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import config from '../../config';
+import {Paper} from "material-ui";
+import logo from './logo.svg';
+import {grey700} from "material-ui/styles/colors"
+
+const style = {
+    height: 150,
+    width: 200,
+    margin: 0,
+};
+
 
 // @flow
 class TopBar extends Component {
@@ -21,9 +32,9 @@ class TopBar extends Component {
     handleClose = () => this.setState({open: false});
 
     render() {
-        const menuItems = this.props.menuItems.filter(it => !it.props.exact)
+        const menuItems = this.props.menuItems
             .map((route, index) => (
-                <MenuItem onTouchTap={this.handleClose} key={index}>
+                <MenuItem className="menu-item" onTouchTap={this.handleClose} key={index}>
                     <Link to={route.props.path}>{route.props.label}</Link>
                 </MenuItem>
             ));
@@ -31,8 +42,21 @@ class TopBar extends Component {
         return (
             <div>
                 <AppBar title={config.appName} iconClassNameRight="muidocs-icon-navigation-expand-more" onTouchTap={this.openDrawer}/>
-                <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
-                    {menuItems}
+                <Drawer docked={false} width={250} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+                    <Grid>
+                        <Row>
+                            <Col>
+                                <Paper className="profile-container" style={{backgroundColor: grey700}} zDepth={2}>
+                                    <img className="logo" src={logo} alt="Logo" />
+                                </Paper>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                {menuItems}
+                            </Col>
+                        </Row>
+                    </Grid>
                 </Drawer>
             </div>
         );
--- src/config.js
+++ src/config.js
@@ -12,12 +12,8 @@ import BusinessNetwork from "./Components/BusinessNetwork/BusinessNetwork";
 export default {
     appName: "Let's net!",
     menu: [
-        <Route key="1" exact path="/" component={Home}/>,
-        <Route key="2" path="/about" component={About} label="About"/>,
-        <Route key="3" path="/contacts" component={Contacts} label="Contacts"/>,
-        <Route key="4" path="/agenda" render={(props) => <Agenda {...props} userName="Dan"/>} label="Agenda"/>,
-        <Route key="5" path="/scanner" render={(props) => <Scanner {...props} onScaned={console.log}/>} label="Scanner"/>,
-        <Route key="6" path="/network" component={BusinessNetwork} label="Business Network"/>,
-        <Route key="7" path="/ticket" render={(props) => <Ticket {...props} name="Bartosz"/>} label="Ticket"/>,
+        <Route key="1" exact path="/" component={Home} label="Ongoing event"/>,
+        <Route key="2" path="/ticket" render={(props) => <Ticket {...props} name="Bartosz"/>} label="Ticket"/>,
+        <Route key="4" path="/network" component={BusinessNetwork} label="Business Network"/>
     ]
 }
