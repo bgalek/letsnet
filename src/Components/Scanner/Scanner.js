2ab82d72e20ba6a5a784172c807d0f49e82ce38b
Krzysztof Szporer <krzysiek.szporer@gmail.com>
2017-06-04 11:30:00 +0200

    Resolved merge conflicts.

--- src/Components/Scanner/Scanner.js
+++ src/Components/Scanner/Scanner.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import QrReader from 'react-qr-reader'
 import {Col, Row, Grid} from "react-flexbox-grid";
+import './Scanner.css';
+import {Avatar, Dialog, FlatButton} from "material-ui";
+import Person from "../../Models/Person";
 
 class Scanner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: new Person(),
+            open: false
+        }
+    }
+
+    handleOpen = (data) => {
+        const person = new Person(data.name, data.surname, data.email);
+        this.setState({open: true, person: person});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
 
     handleScan(data) {
-        if (data) this.props.onScaned(JSON.parse(data));
+        if (data) {
+            let person = JSON.parse(data);
+            this.handleOpen(person);
+        }
     }
 
     handleError(err) {
@@ -14,11 +36,29 @@ class Scanner extends Component {
     }
 
     render() {
+
+        const title = 'Say hello to ' + this.state.person.firstName + '!';
+
+        const actions = [
+            <FlatButton
+                label="Ignore"
+                primary={true}
+                onTouchTap={this.handleClose}/>,
+            <FlatButton
+                label="Accept"
+                primary={true}
+                keyboardFocused={true}
+                onTouchTap={this.handleClose}/>,
+        ];
+
+        console.log(this.state.person);
+
         return (
             <Grid fluid>
                 <Row>
                     <Col xs={12}>
-                        <p>Scan business card here:</p>
+                        <div className="viewfinder">
+                        </div>
                         <QrReader
                             delay={200}
                             style={{width: '100%'}}
@@ -26,6 +66,26 @@ class Scanner extends Component {
                             onScan={(data) => this.handleScan(data)}
                             facingMode={'rear'}
                         />
+                        <Dialog className="dialog"
+                                title={'Say hello to ' + this.state.person.firstName + '!'}
+                                actions={actions}
+                                modal={false}
+                                open={this.state.open}
+                                onRequestClose={this.handleClose}>
+                            <Avatar className="avatar"
+                                    size={50}
+                            />
+                            <section className="user-info">
+                                <h4>Krzysztof Szporer</h4>
+                                <p>Software Developer</p>
+
+                                <ul className="user-list">
+                                    <li>Bosch</li>
+                                    <li>+48999000111</li>
+                                    <li>email@email.com</li>
+                                </ul>
+                            </section>
+                        </Dialog>
                     </Col>
                 </Row>
             </Grid>
--- src/Models/Person.js
+++ src/Models/Person.js
@@ -1,11 +1,32 @@
-export default class Person {
-    constructor( firstName, surname, email ) {
-        this.firstName = firstName;
-        this.surname = surname;
-        this.email = email;
+class Person {
+
+    constructor(firstName, surname, email, company, position) {
+        this._firstName = firstName;
+        this._surname = surname;
+        this._email = email;
+        this._company = company;
+        this._position = position;
     }
 
-    get name() {
-        return this.firstName;
+    get firstName() {
+        return this._firstName;
+    }
+
+    get surname() {
+        return this._surname;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get company() {
+        return this._company;
+    }
+
+    get position() {
+        return this._position;
     }
 }
+
+export default Person
